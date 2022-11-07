e082137a19b447ba47f00b21e77012ac6e753b51
Pranav Mathur <pranavmathur2.0@gmail.com>
2022-11-07 23:50:33 +0530

    done till question 7

---
+++ variable.js
@@ -0,0 +1,50 @@
+/*    1 typeof
+var dstr = "THIS IS A DEMO!";
+var dbool = false;
+var und;
+var dnl = null;
+
+console.log("Variable " + dstr + " is of type " + typeof dstr);
+console.log("Variable " + dbool + " is of type " + typeof dbool);
+console.log("Variable " + und + " is of type " + typeof und);
+console.log("Variable " + dnl + " is of type " + typeof dnl);
+*/
+
+/*    2 interpolation
+var fname = "PRANAV";
+var lname = "MATHUR";
+var married = false;
+var country = "INDIA";
+var age = 31;
+
+console.log(
+  `Hi, my name is ${fname} ${lname}. My martial status is ${married}, I belong to ${country} and am ${age} years old`
+);
+*/
+
+/*    3 toUpperCase
+var lstr =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+console.log(lstr.toUpperCase());
+*/
+
+/*    4 includes
+var str4 = "I love JavaScript, the best programming language!";
+console.log(str4.includes("Script"));
+*/
+
+/*    5 split
+console.log(str4.split(" "));
+*/
+
+/*    6 split
+var silicon = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+silicon = silicon.split(", ");
+console.log(silicon);
+*/
+
+/*    7 lastIndexOf
+var str7 =
+  "Betty Botter bought some butter But she said the butter's bitter If I put it in my batter, it will make my batter bitter But a bit of better butter will make my batter better So `twas better Betty Botter bought a bit of better butter";
+console.log(str7.lastIndexOf("butter"));
+*/
